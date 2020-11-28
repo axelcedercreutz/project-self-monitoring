2ec3ea848315c7c81c0a14dccb9c15d5fdb22436
Axel Cedercreutz <axelcedercreutz@users.noreply.github.com>
2020-11-28 13:16:32 +0200

    push stuff

--- database/database.js
+++ database/database.js
@@ -3,7 +3,21 @@ import { Client } from "../deps.js";
 import { dbconfig } from "../config/config.js";
 
 const getClient = () => {
+  const DATABASE_URL = Deno.env.toObject().DATABASE_URL;
+  if(DATABASE_URL) {
+    const client = new Client(DATABASE_URL);
+    return client;
+  }
   return new Client(dbconfig.database);
+}
+
+const getPort = () => {
+  let port = 7777;
+  if (Deno.args.length > 0) {
+    const lastArgument = Deno.args[Deno.args.length - 1];
+    port = Number(lastArgument);
+  }
+  return port;
 }
 
 const executeQuery = async(query, ...args) => {
@@ -18,4 +32,4 @@ const executeQuery = async(query, ...args) => {
   }
 }
 
-export { executeQuery };
+export { executeQuery, getPort };
